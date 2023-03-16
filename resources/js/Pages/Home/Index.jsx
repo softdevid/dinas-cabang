29b4fd839895e0d275e328e9a9cf84252685018e
Ardiputra614 <ardianto8306@gmail.com>
2023-03-16 11:58:42 +0700

    first commit

---
+++ resources/js/Pages/Home/Index.jsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+const Index = (props) => {
+    return (
+        <>
+            <Head title={props.title} />
+            Halaman Utama
+        </>
+    )
+}
+
+export default Index;
